1c4560d6b2fad9d9d484b49da0921aa01b18568d
mi759 <68837796+mi759@users.noreply.github.com>
2025-01-10 22:18:53 +0900

    Update translation table js to keep checked radio

--- static/js/translation_table.js
+++ static/js/translation_table.js
@@ -1,41 +1,77 @@
 /* ラジオボタンで原文のみ表示を選んだとき */
-function srcOnly(){
+function srcOnly() {
+    /* localStorageに選択状況を保持 */
+    localStorage.setItem("translation-table-radio", "src-only");
     /* sourceクラスのセル全てを表示する */
-    var srcElements = document.getElementsByClassName('source')
-    for(i=0; i<srcElements.length; i++){
+    let srcElements = document.getElementsByClassName('source')
+    for (i = 0; i < srcElements.length; i++) {
         srcElements[i].style.display = "table-cell";
     }
     /* targetクラスのセル全てを非表示にする */
-    var tgtElements = document.getElementsByClassName('target')
-    for(i=0; i<tgtElements.length; i++){
+    let tgtElements = document.getElementsByClassName('target')
+    for (i = 0; i < tgtElements.length; i++) {
         tgtElements[i].style.display = "none";
     }
 }
 
 /* ラジオボタンで両言語表示を選んだとき */
-function srcAndTgt(){
+function srcAndTgt() {
+    /* localStorageに選択状況を保持 */
+    localStorage.setItem("translation-table-radio", "src-tgt");
     /* sourceクラスのセル全てを表示する */
-    var srcElements = document.getElementsByClassName('source')
-    for(i=0; i<srcElements.length; i++){
+    let srcElements = document.getElementsByClassName('source')
+    for (i = 0; i < srcElements.length; i++) {
         srcElements[i].style.display = "table-cell";
     }
     /* targetクラスのセル全てを表示する */
-    var tgtElements = document.getElementsByClassName('target')
-    for(i=0; i<tgtElements.length; i++){
+    let tgtElements = document.getElementsByClassName('target')
+    for (i = 0; i < tgtElements.length; i++) {
         tgtElements[i].style.display = "table-cell";
     }
 }
 
 /* ラジオボタンで訳文のみ表示を選んだとき */
-function tgtOnly(){
+function tgtOnly() {
+    /* localStorageに選択状況を保持 */
+    localStorage.setItem("translation-table-radio", "tgt-only");
     /* sourceクラスのセル全てを非表示にする */
-    var srcElements = document.getElementsByClassName('source')
-    for(i=0; i<srcElements.length; i++){
+    let srcElements = document.getElementsByClassName('source')
+    for (i = 0; i < srcElements.length; i++) {
         srcElements[i].style.display = "none";
     }
     /* targetクラスのセル全てを表示する */
-    var tgtElements = document.getElementsByClassName('target')
-    for(i=0; i<tgtElements.length; i++){
+    let tgtElements = document.getElementsByClassName('target')
+    for (i = 0; i < tgtElements.length; i++) {
         tgtElements[i].style.display = "table-cell";
     }
 }
+
+window.onload = (event) => {
+    let srcOnlyRadio = document.getElementById('src-only');
+    let srcTgtRadio = document.getElementById('src-tgt');
+    let tgtOnlyRadio = document.getElementById('tgt-only');
+    if (srcOnlyRadio === null || srcTgtRadio === null || tgtOnlyRadio === null) {
+        return;
+    }
+
+    let lastSelected = localStorage.getItem("translation-table-radio");
+    switch (lastSelected) {
+        case 'src-only':
+            srcOnlyRadio.checked = true;
+            break;
+        case 'src-tgt':
+            srcTgtRadio.checked = true;
+            break;
+        case 'tgt-only':
+            tgtOnlyRadio.checked = true;
+            break;
+    } 
+
+    if (srcOnlyRadio.checked) {
+        srcOnly();
+    } else if (srcTgtRadio.checked) {
+        srcAndTgt();
+    } else if (tgtOnlyRadio.checked) {
+        tgtOnly();
+    }
+};
